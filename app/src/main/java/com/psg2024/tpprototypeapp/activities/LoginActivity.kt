4b085b15f0011ce1151db57b79ae87e132828eef
Dreamtreeme <k99702@gmail.com>
2024-03-12 15:06:06 +0900

    commit

--- app/src/main/java/com/psg2024/tpprototypeapp/activities/LoginActivity.kt
+++ app/src/main/java/com/psg2024/tpprototypeapp/activities/LoginActivity.kt
@@ -32,17 +32,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //둘러보기 글씨 클릭으로 로그인없이 Main화면으로 이동
-        binding.tvGo.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
+
         //회원가입 버튼 클릭
-        binding.tvSignup.setOnClickListener {
+        binding.btnSignup.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
         }
         //이메일 로그인 버튼 클릭
-        binding.tvEmail.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             startActivity(Intent(this, EmailloginActivity::class.java))
         }
 
