956589bb8cd3edb3507411fb58e24ea04de268b5
Dreamtreeme <k99702@gmail.com>
2024-03-12 17:50:28 +0900

    commit

---
+++ app/src/main/java/com/psg2024/tpprototypeapp/activities/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.psg2024.tpprototypeapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.psg2024.tpprototypeapp.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
