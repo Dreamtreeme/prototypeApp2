fb9775945111d43bf190dd8a3eac1a181ebf366a
Dreamtreeme <k99702@gmail.com>
2024-03-20 17:50:30 +0900

    commit

--- app/src/main/java/com/psg2024/tpprototypeapp/activities/SubMainActivity.kt
+++ app/src/main/java/com/psg2024/tpprototypeapp/activities/SubMainActivity.kt
@@ -3,10 +3,39 @@ package com.psg2024.tpprototypeapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.psg2024.tpprototypeapp.R
+import com.psg2024.tpprototypeapp.databinding.ActivitySubMainBinding
+import com.psg2024.tpprototypeapp.fragments.MapFragment
+import com.psg2024.tpprototypeapp.fragments.PlaceListFragment
+import com.psg2024.tpprototypeapp.fragments.PlaceMapFragment
 
 class SubMainActivity : AppCompatActivity() {
+    val binding by lazy{ActivitySubMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub_main)
+        setContentView(binding.root)
+
+        //처음 보여질 프레그먼트를 화면에 붙이기
+        supportFragmentManager.beginTransaction().add(R.id.container_fragment3, MapFragment())
+            .commit()
+
+        //bnv의 선택에 따라 Fragment를 동적으로 교체.
+
+        binding.bnv.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_bnv_map -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_fragment3, MapFragment()).commit()
+
+                R.id.menu_bnv_addfriend -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_fragment3, PlaceMapFragment()).commit()
+
+                R.id.menu_bnv_list -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_fragment3, PlaceListFragment()).commit()
+
+
+
+            }
+
+            true// OnItemSelectedListener의 추상메소드는 리턴값을 가지고 있음. SAM변환을 하면 return키워드를 사용하면 안됨.
+        }
     }
 }
---
+++ app/src/main/java/com/psg2024/tpprototypeapp/fragments/InviteFragment.kt
@@ -0,0 +1,6 @@
+package com.psg2024.tpprototypeapp.fragments
+
+import androidx.fragment.app.Fragment
+
+class InviteFragment : Fragment() {
+}
